e5a0fba2194240e3c4bd811b5246cea29a411212
psmits <thestinkypete@gmail.com>
2014-01-08 00:10:52 -0600

    bizzare behavior in geom path

--- R/plots.r
+++ R/plots.r
@@ -41,19 +41,23 @@ affcurve <- lapply(affcurve, melt)
 affcurve <- cbind(affcurve$fit, se.fit = affcurve$se.fit$value)
 affcurve[, 1] <- (100 - affcurve[, 1]) / 100
 
-ggaff <- ggplot(affcurve, aes(x = Var1, y = value, colour = Var2)) 
+ggaff <- ggplot(affcurve, aes(x = value, y = Var1, colour = Var2)) 
 ggaff <- ggaff + geom_line()
-ggaff <- ggaff + geom_ribbon(aes(ymin = value - se.fit, ymax = value + se.fit,
-                                 fill = Var2), alpha = 0.3, colour = NA)
-ggaff <- ggaff + labs(x = 'S(t)', y = 'time')
+#ggaff <- ggaff + geom_line(aes(x = value - se.fit, y = Var1, colour = Var2), 
+#                           linetype = 2, alpha = 0.3)
+#ggaff <- ggaff + geom_line(aes(x = value + se.fit, y = Var1, colour = Var2), 
+#                           linetype = 2, alpha = 0.3)
+#ggaff <- ggaff + geom_ribbon(aes(ymin = value - se.fit, ymax = value + se.fit,
+#                                 fill = Var2), alpha = 0.3, colour = NA)
 ggaff <- ggaff + geom_step(data = kmacurve, 
-                           mapping = aes(x = surv, y = time, colour = aff))
+                           mapping = aes(x = time, y = surv, colour = aff))
+ggaff <- ggaff + labs(x = 'time', y = 'S(t)')
 ggaff <- ggaff + scale_fill_manual(values = cbp,
                                    name = 'substrate preference')
 ggaff <- ggaff + scale_colour_manual(values = cbp, 
                                      name = 'substrate preference')
 ggaff <- ggaff + theme(axis.title.y = element_text(angle = 0))
-ggaff <- ggaff + coord_flip()
+#ggaff <- ggaff + coord_flip()
 ggsave(filename = '../doc/figure/aff.png', plot = ggaff)
 
 habcurve <- predict(mode.wei, newdata = data.frame(hab = c('inshore',
@@ -69,17 +73,21 @@ habcurve <- lapply(habcurve, melt)
 habcurve <- cbind(habcurve$fit, se.fit = habcurve$se.fit$value)
 habcurve[, 1] <- (100 - habcurve[, 1]) / 100
 
-gghab <- ggplot(habcurve, aes(x = Var1, y = value, colour = Var2)) 
+gghab <- ggplot(habcurve, aes(x = value, y = Var1, colour = Var2)) 
 gghab <- gghab + geom_line()
-gghab <- gghab + geom_ribbon(aes(ymin = value - se.fit, ymax = value + se.fit,
-                                 fill = Var2), alpha = 0.3, colour = NA)
-gghab <- gghab + labs(x = 'S(t)', y = 'time')
+#gghab <- gghab + geom_line(aes(x = value - se.fit, y = Var1, colour = Var2), 
+#                           linetype = 2, alpha = 0.3)
+#gghab <- gghab + geom_line(aes(x = value + se.fit, y = Var1, colour = Var2), 
+#                           linetype = 2, alpha = 0.3)
+#gghab <- gghab + geom_ribbon(aes(ymin = value - se.fit, ymax = value + se.fit,
+#                                 fill = Var2), alpha = 0.3, colour = NA)
+gghab <- gghab + labs(x = 'time', y = 'S(t)')
 gghab <- gghab + geom_step(data = kmecurve, 
-                           mapping = aes(x = surv, y = time, colour = hab))
-gghab <- gghab + coord_flip()
+                           mapping = aes(x = time, y = surv, colour = hab))
 gghab <- gghab + scale_fill_manual(values = cbp,
                                    name = 'habitat preference')
 gghab <- gghab + scale_colour_manual(values = cbp, 
                                      name = 'habitat preference')
 gghab <- gghab + theme(axis.title.y = element_text(angle = 0))
+#gghab <- gghab + coord_flip()
 ggsave(filename = '../doc/figure/hab.png', plot = gghab)
