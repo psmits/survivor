7e911fa6fe837e201f1482b720c8859308b7b41c
psmits <thestinkypete@gmail.com>
2014-02-15 15:25:45 -0600

    exported governmental geological information

--- R/geology.r
+++ R/geology.r
@@ -1,14 +1,16 @@
-# read in the statigraphic information
-# sort it a lot to get formation level lith
-source('../R/mung.r')
+library(plyr)
+#source('../R/mung.r')
+
+info <- read.csv('../data/psmits-occs.csv', stringsAsFactors = FALSE)
+
 string <- 'Stratigraphic\ names\ Current.txt'
 
-geo <- list.files(path = '../data/geology', pattern = string)
+geo <- list.files(path = '../data/', pattern = string)
 
 states <- list()
 
 for(ii in seq(length(geo))) {
-  states[[ii]] <- read.delim(paste('../data/geology/', geo[ii], sep = ''), sep = '|',
+  states[[ii]] <- read.delim(paste('../data/', geo[ii], sep = ''), sep = '|',
                              stringsAsFactors = FALSE)
 }
 
@@ -44,3 +46,6 @@ ww <- lapply(forms, function(y)
              lapply(matches[coms, 1], function(x, y) grep(y, x), y = y))
 ww <- unlist(lapply(ww, function(x) any(unlist(x) == 1)))
 
+# get the geology out of the matches
+write.table(matches[, c(1, 3, 5, 8, 9, 10, 12, 26, 35, 36, 37)],
+            file = '../data/govt_geology.txt', sep = '|')
