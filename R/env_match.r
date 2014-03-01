3156bb461df0d51eb50f65cf4a208a6ca0c86c1a
psmits <thestinkypete@gmail.com>
2014-03-01 15:35:06 -0600

    paleo env table

---
+++ R/env_match.r
@@ -0,0 +1,53 @@
+library(xtable)
+library(plyr)
+
+gov <- read.csv('../data/geology.csv', stringsAsFactors = FALSE)
+occ <- read.csv('../data/psmits-occs.csv', stringsAsFactors = FALSE)
+
+form <- cbind(occ$geological_group, occ$formation, occ$member)
+
+form <- aaply(form, 1, function(x) gsub(pattern = '[^[:alnum:] ]', '',
+                                        x, perl = TRUE))
+known <- gov$formation[gov$environment1 != '']
+
+# find out which in the pdbd i know about currently
+oo <- list()
+for(ii in seq(ncol(form))) {
+  ll <- list()
+  for(jj in seq(length(known))) {
+    ll[[jj]] <- which(grepl(pattern = known[jj], form[, ii]))
+  }
+  oo[[ii]] <- ll
+}
+oo <- lapply(oo, unlist)
+oo <- oo[[which.max(unlist(lapply(oo, length)))]]
+
+kk <- occ[oo, ]  # this is the data i have environment for currently
+kk <- cbind(kk$formation, kk$lithology1, kk$lithology2, kk$environment)
+kk <- aaply(kk, 1, function(x) gsub(pattern = '[^[:alnum:] ]', '',
+                                    x, perl = TRUE))
+kk <- data.frame(kk[!duplicated(kk[, 1]), ], stringsAsFactors = FALSE)
+
+# combine my info with the pbdb
+mm <- lapply(known, function(x) grep(x, kk[, 1]))
+good <- which(unlist(lapply(mm, any)))
+mm <- mm[good]
+names(mm) <- known[good]
+
+# for each good formation, grab the environmental
+grab <- gov$formation %in% names(mm)
+got <- gov[grab, ]
+got <- got[got$environment1 != '', ]
+got <- data.frame(cbind(got$formation, got$lithology1, got$lithology2, got$environment1, got$environment2), stringsAsFactors = FALSE)
+
+# ok, make a table that has old and new next to each other
+improve <- cbind(kk, got[, -1])
+names(improve) <- c('formation', 'PBDB lithology 1', 'PBDB lithology 2',
+                    'PBDB paleoenvironment', 'my lithology 1', 'my lithology 2',
+                    'my paleoenvironment 1', 'my paleoenvironment 2')
+
+rownames(improve) <- NULL
+improve.table <- xtable(improve)
+label(improve.table) <- 'tab:paleoenv'
+print.xtable(improve.table, file = '../doc/paleoenv_tab.tex', 
+             include.rownames = FALSE)
