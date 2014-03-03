c1a6de953a3a9ad906ff93b1b6ede4b5c76d706c
psmits <thestinkypete@gmail.com>
2014-03-03 15:05:54 -0600

    more paleoenv data

--- R/env_match.r
+++ R/env_match.r
@@ -8,7 +8,7 @@ form <- cbind(occ$geological_group, occ$formation, occ$member)
 
 form <- aaply(form, 1, function(x) gsub(pattern = '[^[:alnum:] ]', '',
                                         x, perl = TRUE))
-known <- gov$formation[gov$environment1 != '']
+known <- unique(gov$formation[gov$environment1 != ''])
 
 # find out which in the pdbd i know about currently
 oo <- list()
@@ -23,7 +23,7 @@ oo <- lapply(oo, unlist)
 oo <- oo[[which.max(unlist(lapply(oo, length)))]]
 
 kk <- occ[oo, ]  # this is the data i have environment for currently
-kk <- cbind(kk$formation, kk$lithology1, kk$lithology2, kk$environment)
+kk <- cbind(kk$formation, kk$environment)
 kk <- aaply(kk, 1, function(x) gsub(pattern = '[^[:alnum:] ]', '',
                                     x, perl = TRUE))
 kk <- data.frame(kk[!duplicated(kk[, 1]), ], stringsAsFactors = FALSE)
@@ -38,15 +38,14 @@ names(mm) <- known[good]
 grab <- gov$formation %in% names(mm)
 got <- gov[grab, ]
 got <- got[got$environment1 != '', ]
-got <- data.frame(cbind(got$formation, got$lithology1, got$lithology2, got$environment1, got$environment2), stringsAsFactors = FALSE)
+got <- got[!duplicated(got$formation), ]
+got <- data.frame(cbind(got$formation, got$environment1, got$environment2), stringsAsFactors = FALSE)
 
 # ok, make a table that has old and new next to each other
 improve <- cbind(kk, got[, -1])
-names(improve) <- c('formation', 'PBDB lithology 1', 'PBDB lithology 2',
-                    'PBDB paleoenvironment', 'my lithology 1', 'my lithology 2',
-                    'my paleoenvironment 1', 'my paleoenvironment 2')
-
+names(improve) <- c('formation', 'PBDB paleoenvironment', 'my paleoenvironment 1', 'my paleoenvironment 2')
 rownames(improve) <- NULL
+improve <- improve[order(improve$formation), ]
 improve.table <- xtable(improve)
 label(improve.table) <- 'tab:paleoenv'
 print.xtable(improve.table, file = '../doc/paleoenv_tab.tex', 
