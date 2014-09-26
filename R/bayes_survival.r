75a244c9a71538e3b5dc7992024efddd44c5cf45
psmits <thestinkypete@gmail.com>
2014-09-26 15:48:24 -0500

    integrating over censored values

--- R/bayes_survival.r
+++ R/bayes_survival.r
@@ -1,6 +1,10 @@
 library(rstan)
+library(parallel)
 
-source('../R/mung.r')
+source('../R/networks.r')
+
+RNGkind(kind = "L'Ecuyer-CMRG")
+seed <- 420
 
 # assemble data
 duration <- surv[, 1]
@@ -9,28 +13,57 @@ extinct <- surv[, 3]  # 1 yes, 0 no
 size <- uni[, 2]
 
 aff <- persist$aff
+hab <- persist$hab
+hab <- (hab == 'inshore') * 1
 
-# this is for the really complicated idea i have about 
-# having affinity as a distribution
-taxa.carb <- unlist(lapply(tocc, function(x) {
-                           if(is.na(x['carbonate'])) 0 else x['carbonate']}))
-taxa.occ <- unlist(lapply(tocc, sum))
-total.carb <- unlist(lapply(kocc, function(x){
-                            if(is.na(x['carbonate'])) 0 else x['carbonate']}))
-total.occ <- unlist(lapply(kocc, sum))
-
-
+occ <- persist$occu
 
 data <- list(duration = duration,
              siz = log(size),
-             aff = aff)
+             aff = aff,
+             occ = occ,
+             hab = hab,
+             extinct = extinct)
 good <- !is.na(duration)
 data <- lapply(data, function(x) x[good])
-data$N <- length(data$duration) 
+
+# uncensored vs censored
+grab <- data$extinct == 1
+unc <- lapply(data, function(x) x[grab])
+cen <- lapply(data, function(x) x[!grab])
+
+data <- list(dur_unc = unc$duration,
+             size_unc = unc$siz,
+             aff_unc = unc$aff,
+             occ_unc = unc$occ,
+             hab_unc = unc$hab,
+             N_unc = length(unc$duration),
+             dur_cen = cen$duration,
+             size_cen = cen$siz,
+             aff_cen = cen$aff,
+             hab_cen = cen$hab,
+             occ_cen = cen$occ,
+             N_cen = length(cen$duration))
+
 
 # compile and fit model
 fit <- stan(file = '../stan/survival_model.stan')
 
-fit1 <- stan(fit = fit, data = data, iter = 1000, chains = 4)
+# parallel magic
+fitlist <- mclapply(1:4, mc.cores = detectCores(),
+                    function(x) stan(fit = fit, seed = seed,
+                                     data = data,
+                                     chains = 1, chain_id = x,
+                                     refresh = -1))
+fit1 <- sflist2stanfit(fitlist)
 
-#plot(density(rweibull(1000, 1.77, 21.6)))
+
+
+# this is for the really complicated idea i have about 
+# having affinity as a distribution
+#taxa.carb <- unlist(lapply(tocc, function(x) {
+#                           if(is.na(x['carbonate'])) 0 else x['carbonate']}))
+#taxa.occ <- unlist(lapply(tocc, sum))
+#total.carb <- unlist(lapply(kocc, function(x){
+#                            if(is.na(x['carbonate'])) 0 else x['carbonate']}))
+#total.occ <- unlist(lapply(kocc, sum))
--- stan/survival_model.stan
+++ stan/survival_model.stan
@@ -1,18 +1,29 @@
 data {
-  int<lower=0> N;
-  vector[N] duration;
-  vector[N] siz;
-  vector[N] aff;
+  int<lower=0> N_unc;
+  int<lower=0> N_cen;
+  real dur_unc[N_unc];
+  real dur_cen[N_cen];
+  vector[N_unc] size_unc;
+  vector[N_cen] size_cen;
+  vector[N_unc] aff_unc;
+  vector[N_cen] aff_cen;
+  vector[N_unc] occ_unc;
+  vector[N_cen] occ_cen;
+  vector[N_unc] hab_unc;
+  vector[N_cen] hab_cen;
 }
 parameters {
-  vector[3] beta;
-  real<lower=0>  alpha;
+  vector[5] beta;
+  real<lower=0> alpha;
 }
 model {
-  beta[1] ~ normal(0, 1000);
-  beta[2] ~ normal(0, 1000);
-  beta[3] ~ normal(0, 1000);
+  beta[1] ~ normal(0, 100);
+  beta[2] ~ normal(0, 100);
+  beta[3] ~ normal(0, 100);
+  beta[4] ~ normal(0, 100);
+  beta[5] ~ normal(0, 100);
   alpha ~ gamma(1, 1);
 
-  duration ~ weibull(alpha, exp(beta[1] + beta[2] * siz + beta[3] * aff));
+  dur_unc ~ weibull(alpha, exp(beta[1] + beta[2] * size_unc + beta[3] * aff_unc + beta[4] * occ_unc + beta[5] * hab_unc));
+  increment_log_prob(N_cen * weibull_ccdf_log(dur_cen, alpha, exp(beta[1] + beta[2] * size_cen + beta[3] * aff_cen + beta[4] * occ_cen + beta[5] * hab_cen)));
 }
